62ecc70fcd4799981a31168e3b5657d4ea1a0196
siaris <wirawan.is@gmail.com>
2025-06-19 17:05:22 +0700

    Update PostgreSQL connection configuration to use environment variable for search path

--- config/database.php
+++ config/database.php
@@ -93,8 +93,7 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
-            'schema' => env('DB_SCHEMA', 'public'),
+            'search_path' => env('DB_SCHEMA', 'public'),
             'sslmode' => 'prefer',
         ],
 
