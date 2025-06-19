f3b1628fe80445cce5bd0ce986f68630d84e004a
siaris <wirawan.is@gmail.com>
2025-06-19 17:05:38 +0700

    Refactor User model: update fillable attributes and remove unused fields

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    protected $table = 'user';
 
     /**
      * The attributes that are mass assignable.
@@ -18,8 +19,9 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
         'email',
+        'lastname',
         'password',
     ];
 
@@ -30,7 +32,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -41,7 +42,8 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+            'created' => 'datetime',
+            'updated' => 'datetime',
             'password' => 'hashed',
         ];
     }
