b066b506e702fa53704fe4e1dc91ca958c611806
siaris <wirawan.is@gmail.com>
2025-07-03 16:59:33 +0700

    Add MasterFormularium and MasterSatuan models with table and fillable properties

---
+++ app/Models/MasterFormularium.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MasterFormularium extends Model
+{
+    protected $table = 'master_formularium';
+    protected $fillable = ['nama'];
+}
---
+++ app/Models/MasterSatuan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MasterSatuan extends Model
+{
+    //
+    protected $table = 'master_satuan';
+    protected $fillable = ['nama'];
+}
